554208614f2063e484bbd2b80a3d325e882392b2
HAPPY YADAV <happy.hy0017@gmail.com>
2024-05-17 19:25:00 +0530

    initialized swaadguru

---
+++ main.js
@@ -0,0 +1,21 @@
+let menu =  document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    }
+    window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+        if(window.scrollY > 60){
+            document.querySelector('#scroll-top').classList.add('active');}
+            else{
+                document.querySelector('#scroll-top').classList.remove('active');}
+        }
+ function loader(){
+    document.querySelector('.loader-container').classList('fade-out');
+ }   
+ function fadeOut(){
+    setInterval(loader, 3000);
+ }
+ window.onload = fadeOut();
